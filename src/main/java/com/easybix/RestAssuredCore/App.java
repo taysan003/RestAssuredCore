6cd2ce42da6cd00c3cf14cf1313ed3161feb9fa8
Andrei <taysan003@gmail.com>
2018-09-18 16:11:55 -0400

    first commit

---
+++ src/main/java/com/easybix/RestAssuredCore/App.java
@@ -0,0 +1,13 @@
+package com.easybix.RestAssuredCore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
